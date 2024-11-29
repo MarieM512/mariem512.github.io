50c8064b1c4d5b9f3dee92d63b7bce7b1d9cd511
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-11-29 22:22:06 +0100

    Complete refonte (#4)

--- app.vue
+++ app.vue
@@ -5,38 +5,19 @@
       <personal-info/>
     </div>
 
-    <div class="col-span-3 row-span-3">
-      <UCard>
-        <UTabs :items="items">
-          <template #aboutMe="{item}">
-            <about-me/>
-          </template>
-        </UTabs>
-      </UCard>
-    </div>
-
-    <div class="col-span-3 row-span-1">
+    <div class="col-span-3 row-span-2">
       <skill/>
     </div>
+
+    <div class="col-span-3 row-span-2">
+      <portfolio/>
+    </div>
+
   </div>
 </template>
 
 <script setup lang="ts">
-import AboutMe from "~/component/aboutMe.vue";
 import PersonalInfo from "~/component/personalInfo.vue";
 import Skill from "~/component/skill.vue";
-
-const items = [{
-  label: 'À propos de moi',
-  icon: 'i-carbon:user',
-  slot: 'aboutMe'
-}, {
-  label: 'Mon parcours',
-  icon: 'i-carbon:3d-curve-auto-colon',
-  content: 'Cet espace est en cours de développement'
-}, {
-  label: 'Portfolio',
-  icon: 'i-carbon:application-mobile',
-  content: 'Cet espace est en cours de développement'
-}]
+import Portfolio from "~/component/portfolio.vue";
 </script>
