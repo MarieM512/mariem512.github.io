5aa0e0a6809517ecfdee88c84dee3693d7fd5fd4
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-12-19 23:11:32 +0100

    Feat/skill (#6)
    * Add all skills
    
    * Add responsive for personal
    
    * Add skill card

--- component/skill.vue
+++ component/skill.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-
+import SkillCard from "~/component/SkillCard.vue";
 </script>
 
 <template>
-  <UCard class="h-full w-full">
-    <p>Skill</p>
-  </UCard>
+    <UCard class="flex h-full w-full overflow-y-auto">
+      <p class="w-full flex justify-center text-2xl p-4">Compétences</p>
+      <div class="flex flex-wrap justify-center gap-4 container">
+        <SkillCard class="col" name="Swift"></SkillCard>
+        <SkillCard name="Kotlin"></SkillCard>
+        <SkillCard name="Flutter"></SkillCard>
+        <SkillCard name="Angular"></SkillCard>
+        <SkillCard name="React"></SkillCard>
+        <SkillCard name="Vue"></SkillCard>
+        <SkillCard name="Java"></SkillCard>
+        <SkillCard name="Lua"></SkillCard>
+      </div>
+    </UCard>
 </template>
 
 <style scoped>
