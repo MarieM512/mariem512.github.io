2092e752ffe503f7eb8c4948b14adcc29a4fcb12
Marie Metay <mariemetay.pro@gmail.com>
2024-12-22 01:35:34 +0100

    Add padding to skill card

--- component/skill.vue
+++ component/skill.vue
@@ -5,7 +5,7 @@ import SkillCard from "~/component/SkillCard.vue";
 <template>
     <UCard class="flex h-full w-full overflow-y-auto">
       <p class="w-full flex justify-center text-2xl p-4">Compétences</p>
-      <div class="flex flex-wrap justify-center gap-4 container">
+      <div class="flex flex-wrap justify-center gap-4 container p-2">
         <SkillCard class="col" name="Swift"></SkillCard>
         <SkillCard name="Kotlin"></SkillCard>
         <SkillCard name="Flutter"></SkillCard>
