12eb8c82e168af73446e1a1bba8e01ecf0b83ef0
Marie Metay <118732275+MarieM512@users.noreply.github.com>
2024-12-22 19:59:32 +0100

    Feat/portfolio (#8)
    * Add padding to skill card
    
    * Add y scroll for personal
    
    * Add portfolio

--- component/portfolio.vue
+++ component/portfolio.vue
@@ -1,13 +1,23 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <UCard class="h-full w-full">
-    <p>Portfolio</p>
+  <UCard class="flex h-full w-full justify-center overflow-y-auto">
+    <p class="flex justify-center text-2xl p-4">Portfolio</p>
+
+    <div class="flex flex-wrap justify-center gap-4 container p-2">
+      <UCard>
+        <div class="p-4 flex flex-col justify-center gap-2">
+          <USkeleton class="h-20 w-20" />
+          <p class="text-center">CatBreeds</p>
+          <UBadge color="purple" class="gap-2 p-2 justify-center">
+            <UIcon name="i-simple-icons-apple" />
+            <p>iOS</p>
+          </UBadge>
+        </div>
+      </UCard>
+      
+    </div>
   </UCard>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
