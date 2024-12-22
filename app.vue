9c261b829ff5b3647bc4e0f10273f78eba065b71
Marie Metay <mariemetay.pro@gmail.com>
2024-12-22 22:24:33 +0100

    Add limite for large screen

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-cols-1 md:grid-cols-3 md:grid-rows-4 h-screen w-screen gap-4 p-4"
+    class="grid grid-cols-1 md:grid-cols-3 md:grid-rows-4 h-screen w-screen gap-4 p-4 max-w-screen-2xl mx-auto"
   >
     <div class="col-span-2 md:col-span-1 row-span-4">
       <personal />
@@ -15,19 +15,6 @@
     >
       <portfolio />
     </div>
-
-    <!-- <div
-      class="col-span-2 row-span-2 flex"
-      ref="target"
-      :style="cssVars"
-      :class="['rounded-[15px] p-[2px] shine', props.class || '']"
-    >
-      <div
-        class="rounded-[13px] w-full h-full bg-gradient-to-b from-neutral-800/50 to-neutral-950/50 bg-neutral-950/80"
-      >
-        <portfolio />
-      </div>
-    </div> -->
   </div>
 </template>
 
