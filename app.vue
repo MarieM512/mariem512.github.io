922fdeb1008ed5d34edb5c1d918569164f40437f
Marie Metay <mariemetay.pro@gmail.com>
2025-02-04 19:31:44 +0100

    fix: border

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="grid grid-cols-1 md:grid-cols-3 md:grid-rows-4 h-screen w-screen gap-4 p-4 max-w-screen-2xl mx-auto"
+    class="grid grid-cols-1 md:grid-cols-3 md:grid-rows-4 h-screen w-screen gap-4 rounded-[15px] p-[2px] shine rounded-[13px] p-4"
   >
     <div class="col-span-2 md:col-span-1 row-span-4">
       <personal />
