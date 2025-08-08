4e068204177f901ef38a7d9d3217897379aabc26
Marie Metay <mariemetay.pro@gmail.com>
2025-08-08 20:02:08 +0200

    chore: remove base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     modules: ['@nuxt/ui'],
-    app: {
-        baseURL: '/mariem512.github.io/',  // Remplace NOM_DU_REPO par le nom exact de ton repo GitHub
-    },
     nitro: {
         preset: 'github_pages',
     },
