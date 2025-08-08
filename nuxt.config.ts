439e2e2993df036cae600c277bb651e8c6a30b0e
Marie Metay <mariemetay.pro@gmail.com>
2025-08-08 19:49:03 +0200

    chore: modify ci

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['@nuxt/ui'],
+    app: {
+        baseURL: '/mariem512.github.io/',  // Remplace NOM_DU_REPO par le nom exact de ton repo GitHub
+    },
+    nitro: {
+        preset: 'github_pages',
+    },
 })
